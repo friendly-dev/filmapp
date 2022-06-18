b485c93abe32aadf30c3fc6799592e54a199be8a
Lenovo <fidim12@mail.ru>
2022-06-18 17:24:50 +0300

    new commit

--- practice/js/script.js
+++ practice/js/script.js
@@ -67,12 +67,14 @@ const writeYourGenres = () => {
 
 writeYourGenres();
 
+let arr = [2, 4, 6, 8, 10];
 
-/* Задание на урок:
-1) Первую часть задания повторить по уроку
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-P.S. Функции вызывать не обязательно */
+// for (let i = 0; i < arr.length; i++ ) {
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function (item, index, arr){
+    console.log(index);
+    console.log(item);
+    console.log(arr);
+});
